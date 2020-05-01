b48605cbd1a9f89ddb8383a4e33bf0fd09c8aca7
Bezymyanyj <Bezymyanyj@gmail.com>
2020-04-30 20:07:10 -0400

    Bones Controller

---
+++ Runner_Module_13_18/Assets/Scripts/BonesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonesController : MonoBehaviour
+{
+    public Collider[] colliders;
+
+
+
+    public void TurnOnTrigers()
+    {
+        for (int i = 0; i < colliders.Length - 1; i++)
+        {
+            colliders[i].isTrigger = false;
+        }
+    }
+
+}
