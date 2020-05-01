0588f77febc59f5a01cf960ee785cb088bf67056
Bezymyanyj <Bezymyanyj@gmail.com>
2020-04-30 20:06:58 -0400

    New Triger Obstacle

---
+++ Runner_Module_13_18/Assets/Scripts/TrigerSphere.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigerSphere : MonoBehaviour
+{
+    public GameObject obstacle;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            obstacle.SetActive(true);
+        }
+    }
+}
