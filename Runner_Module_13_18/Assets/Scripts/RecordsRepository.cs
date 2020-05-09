7a635f50c19d07b0db3a7fcea0f0489802ffadc4
Bezymyanyj <Bezymyanyj@gmail.com>
2020-05-08 21:57:15 -0400

    UI plus Repository

---
+++ Runner_Module_13_18/Assets/Scripts/RecordsRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class RecordsRepository : MonoBehaviour
+{
+    private string distance;
+
+    private string ballCount;
+
+    private string path = @"data.csv";
+
+    private string[] titles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadRepository()
+    {
+        if (File.Exists(path))
+        {
+            this.Load(); // Загрузка данных
+        }
+        else Save(path);
+    }
+
+
+    public void Save(string path)
+    {
+        distance = $"{UIController.Instance.Distance.ToString()}";
+        ballCount = $"{UIController.Instance.CountBalls.ToString()}";
+        File.AppendAllText(path, $"{distance}, {ballCount}\n");
+    }
+
+    private void Load()
+    {
+        using (StreamReader sr = new StreamReader(this.path))
+        {
+            titles = sr.ReadLine().Split(',');
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] args = sr.ReadLine().Split(',');
+
+                UIController.Instance.topList[i] = $"{i}. {args[0]}, {args[1]}\n";
+                UIController.Instance.results[i] = float.Parse(args[0]);
+            }
+        }
+    }
+}
---
+++ Runner_Module_13_18/Assets/Scripts/UIController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : SingletonAsComponent<UIController>
+{
+    public static UIController Instance
+    {
+        get { return ((UIController)_Instance); }
+        set { _Instance = value; }
+    }
+
+    public GameObject pausePanel;
+    public GameObject topPanel;
+    public GameObject fallPanel;
+    public Text[] distanceCount;
+    public Text[] ballCount;
+    private RecordsRepository rep;
+
+    private bool isPause = false;
+    private bool isPlaying = true;
+    public string[] topList { get; set; }
+    public float[] results { get; set; }
+    public int CountBalls { get; set; }
+    public float Distance { get; set; }
+
+    private void Start()
+    {
+        rep = GetComponent<RecordsRepository>();
+    }
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            distanceCount[0].text = $"{Distance.ToString("#")}";
+            distanceCount[1].text = $"Distance: {Distance.ToString("#")}";
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                isPause = !isPause;
+            }
+
+            Time.timeScale = isPause ? 0 : 1;
+            Cursor.lockState = isPause ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = isPause ? true : false;
+            pausePanel.SetActive(isPause ? true : false);
+        }
+    }
+
+    public void ExitApplication()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void UpdateBallText()
+    {
+        for (int i = 0; i < ballCount.Length; i++)
+        {
+            ballCount[i].text = $"Balls: {CountBalls}";
+        }
+    }
+
+    public void GoToTopMenu()
+    {
+        fallPanel.SetActive(false);
+        topPanel.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        fallPanel.SetActive(true);
+        topPanel.SetActive(false);
+    }
+
+    public void Fall()
+    {
+        isPlaying = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        fallPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Runner");
+    }
+}
