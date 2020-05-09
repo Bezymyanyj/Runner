cbfc52936a7c0538204ea5bc96646af8a41a44aa
Bezymyanyj <Bezymyanyj@gmail.com>
2020-05-08 21:55:28 -0400

    Main menu controller

---
+++ Runner_Module_13_18/Assets/Scripts/MainUiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUiController : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject topPanel;
+
+
+    public void GoToTopMenu()
+    {
+        mainPanel.SetActive(false);
+        topPanel.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+
+        mainPanel.SetActive(true);
+        topPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Runner");
+    }
+
+    public void ExitApplication()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
