4ca22fdb3606973d55fdc74866e20be7f79f5cc6
Bezymyanyj <Bezymyanyj@gmail.com>
2020-05-11 20:13:58 -0400

    UI

--- Runner_Module_13_18/Assets/Scripts/RecordsRepository.cs
+++ Runner_Module_13_18/Assets/Scripts/RecordsRepository.cs
@@ -6,55 +6,66 @@ using UnityEngine;
 
 public class RecordsRepository : MonoBehaviour
 {
-    private string distance;
+    private string distance = "10";
 
-    private string ballCount;
+    private string ballCount = "0";
 
-    private string path = @"data.csv";
+    private string path = "/data.csv";
 
-    private string[] titles;
+    private int index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        LoadRepository();
     }
 
     private void LoadRepository()
     {
-        if (File.Exists(path))
+        if (File.Exists(Application.dataPath + path))
         {
+            Debug.Log("Load File");
             this.Load(); // Загрузка данных
         }
-        else Save(path);
+        else
+        {
+            CreateFile(path);
+            Debug.Log("Create New File");
+        }
+    }
+
+    public void Save()
+    {
+        File.Delete(Application.dataPath + path);
+
+        for (int i = 0; i < UIController.Instance.Balls.Count; i++)
+        {
+            File.AppendAllText(Application.dataPath + path, $"{UIController.Instance.Results[i].ToString("#")}, {UIController.Instance.Balls[i]}\n");
+        }
     }
 
 
-    public void Save(string path)
+    private void CreateFile(string path)
     {
-        distance = $"{UIController.Instance.Distance.ToString()}";
-        ballCount = $"{UIController.Instance.CountBalls.ToString()}";
-        File.AppendAllText(path, $"{distance}, {ballCount}\n");
+        for (int i = 0; i < 10; i++)
+        {
+            File.AppendAllText(Application.dataPath + path, $"{distance}, {ballCount}\n");
+        }
     }
 
     private void Load()
     {
-        using (StreamReader sr = new StreamReader(this.path))
+        using (StreamReader sr = new StreamReader(Application.dataPath + path))
         {
-            titles = sr.ReadLine().Split(',');
-
-            for (int i = 0; i < 10; i++)
+            while (!sr.EndOfStream)
             {
+                //Debug.Log("Read Line Index:" + index);
                 string[] args = sr.ReadLine().Split(',');
-
-                UIController.Instance.topList[i] = $"{i}. {args[0]}, {args[1]}\n";
-                UIController.Instance.results[i] = float.Parse(args[0]);
+                
+                UIController.Instance.Results.Add(index, float.Parse(args[0]));
+                UIController.Instance.Balls.Add(index, int.Parse(args[1]));
+                index++;
             }
         }
+        index = 0;
     }
 }
--- Runner_Module_13_18/Assets/Scripts/UIController.cs
+++ Runner_Module_13_18/Assets/Scripts/UIController.cs
@@ -17,12 +17,19 @@ public class UIController : SingletonAsComponent<UIController>
     public GameObject fallPanel;
     public Text[] distanceCount;
     public Text[] ballCount;
+    public Text TopList;
     private RecordsRepository rep;
 
+    private bool isFall = false;
     private bool isPause = false;
     private bool isPlaying = true;
-    public string[] topList { get; set; }
-    public float[] results { get; set; }
+    private string topResult;
+    private string numberTitle = "Number";
+    private string distanceTitle = "Distance";
+    private string ballsTitle = "Balls";
+    private int index = 1;
+    public Dictionary<int, int> Balls = new Dictionary<int, int>();
+    public Dictionary<int, float> Results = new Dictionary<int, float>();
     public int CountBalls { get; set; }
     public float Distance { get; set; }
 
@@ -34,8 +41,10 @@ public class UIController : SingletonAsComponent<UIController>
     {
         if (isPlaying)
         {
+            
             distanceCount[0].text = $"{Distance.ToString("#")}";
             distanceCount[1].text = $"Distance: {Distance.ToString("#")}";
+            distanceCount[2].text = $"Distance: {Distance.ToString("#")}";
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 isPause = !isPause;
@@ -51,6 +60,7 @@ public class UIController : SingletonAsComponent<UIController>
     public void ExitApplication()
     {
         Debug.Log("Exit");
+        rep.Save();
         Application.Quit();
     }
 
@@ -65,6 +75,7 @@ public class UIController : SingletonAsComponent<UIController>
     public void GoToTopMenu()
     {
         fallPanel.SetActive(false);
+        PrintTopResults();
         topPanel.SetActive(true);
     }
 
@@ -76,14 +87,63 @@ public class UIController : SingletonAsComponent<UIController>
 
     public void Fall()
     {
-        isPlaying = false;
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        fallPanel.SetActive(true);
+        if (!isFall)
+        {
+            isFall = true;
+            isPlaying = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            CheckBestResult(Distance, CountBalls);
+            fallPanel.SetActive(true);
+        }
     }
 
     public void RestartGame()
     {
+        rep.Save();
         SceneManager.LoadScene("Runner");
+    }
+
+    private void PrintTopResults()
+    {
+        TopList.text = "";
+        string title = $"{numberTitle,5} {distanceTitle,25} {ballsTitle,10}\n";
+        TopList.text += title;
+        for (int i = 0; i < Results.Count; i++)
+        {
+            string dictance = Results[i].ToString("#");
+            TopList.text += string.Format($"{index++,5}. {dictance,25} {Balls[i],15}\n");
+        }
+        index = 1;
+        //Debug.Log(TopList.text);
+    }
+
+    private void CheckBestResult(float distance, int balls)
+    {
+        //Debug.Log("How many times, I write record?");
+        for (int i = 0; i < Results.Count; i++)
+        {
+            if (distance > Results[i])
+            {
+                int topTmp = Balls[i];
+                Balls[i] = balls;
+                balls = topTmp;
+                float tmp = Results[i];
+                Results[i] = distance;
+                distance = tmp;
+            }
+        }
+    }
+
+    private void PrintToLog()
+    {
+        foreach(KeyValuePair<int, int> i in Balls)
+        {
+            Debug.Log(i.Value);
+        }
+        foreach(KeyValuePair<int, float > i in Results)
+        {
+            Debug.Log(i.Value);
+        }
     }
 }
